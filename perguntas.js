25b634f25ffaf4c1a75c96ddcebe50a4919781cb
EmilMinigamer <emilia.ramos.silva@escola.pr.gov.br>
2024-10-13 21:12:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'DESENHO',
+    'Qual é a história de Regular Show?',
+    'A série gira em torno da vida de dois amigos, que trabalham em um parque. Geralmente tentam resolver problemas simples que os leva a uma desventura surreal.'
+)
+
+criaCartao(
+    'DESENHO',
+    'Qual é o poder do Jake em Adventure Time?',
+    'Jake tem a capacidade de mudar de forma, o que significa que ele pode controlar o tamanho, forma e dimensões do seu corpo.'
+)
+
+criaCartao(
+    'SÉRIE',
+    'Em qual ano Stranger Things se passa?',
+    'A série se passa durante os anos 80.'
+)
+
+criaCartao(
+    'GAMES',
+    'Em qual ano Tetris foi lançado? e por quem?',
+    'Tetris é um jogo eletrônic de quebra-cabeça que foi lançado em 1985, por Alexey Pajitnov.'
+)
+
+criaCartao(
+    'ANIME',
+    'Qual é a técnica utilizada pelo goku, que envolve a concentração de energia em suas mãos, liberando-o em força de rajada?',
+    'A técnica ultilizada pelo goku, é conhecida como "Kamehameha".'
+)
+
+criaCartao(
+    'FILME',
+    'Como foi feito Coraline?',
+    'Foi feito em stop motion, uma técnica que consiste em tirar diversas fotos de bonecos inanimados para montar as cenas.'
+)
+
+criaCartao(
+    'ANIME',
+    "O que são os Stands em Jojo's bizarre Adventure?",
+    'Os Stands são a manifestação física da "energia vital" de seu usuário.'
+)
+
+criaCartao(
+    'MÚSICA',
+    'Qual o significado da música "Staiway to heaven" de Led Zeppelin?',
+    'A "Stairway to heaven"(escada para o céu) pode ser vista como uma representação da jornada espiritual ou da busca pela iluminação.'
+)
+
+criaCartao(
+    'GAMES',
+    'Qual é a história de Omori?',
+    'O jogo sobre um garoto e seu alter-ego que sofreu um trauma e ficou em quarentena por 4 anos.'
+)
